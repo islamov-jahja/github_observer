cb80a40dabc807805a9b4f3e9b6b56b74d069f2d
islamov <yahyaislamov15@mail.ru>
2021-08-06 01:34:56 +0300

    refactoring

--- backend/app/package/listRepositories/adapter/repositories/GithubRepositoriesFromApiRepository.php
+++ backend/app/package/listRepositories/adapter/repositories/GithubRepositoriesFromApiRepository.php
@@ -1,9 +1,8 @@
 <?php
 
-
 namespace app\components\repository;
 
-
+use app\githubObserver\common\enums\DateFormat;
 use app\githubObserver\listRepositories\domain\entity\interfaces\IGithubRepositoryEntity;
 use app\githubObserver\listRepositories\domain\entity\interfaces\IGithubUserEntity;
 use app\components\repository\interfaces\IGithubRepositoriesRepository;
@@ -12,48 +11,52 @@ use yii\httpclient\Client;
 
 class GithubRepositoriesFromApiRepository implements IGithubRepositoriesRepository
 {
-    private Client $client;
+
     public const GITHUB_API_URL = "https://api.github.com";
 
+    /** @var \yii\httpclient\Client */
+    private Client $client;
+
+    /**
+     * @param  \yii\httpclient\Client  $client
+     */
     public function __construct(Client $client)
     {
         $this->client = $client;
     }
 
     /**
-     * @param IGithubUserEntity|null $user
+     * @param  IGithubUserEntity|null  $user
+     *
      * @return GithubRepos[]
-     * @throws \yii\base\InvalidConfigException
      * @throws \yii\httpclient\Exception
      */
     public function get(?IGithubUserEntity $user): array
     {
         $repositories = [];
 
-        if ($user === null){
+        if ($user === null) {
             return $repositories;
         }
 
-        $url = self::GITHUB_API_URL. "/users/{$user->getName()}/repos";
+        $url      = self::GITHUB_API_URL . "/users/{$user->getName()}/repos";
         $response = $this->client
             ->get($url)
             ->addHeaders(['User-Agent' => 'Personal-Computer'])
             ->setFormat(Client::FORMAT_JSON)
             ->send();
 
-        foreach ($response->data as $item){
-            $githubRepos = new GithubRepos([
-                'name' => $item['name'] ?? null,
-                'github_user_id' => $user->getId(),
-                'link' => $item['html_url']
-            ]);
-
-            $githubRepos->setUpdateDateTime(new \DateTime($item['updated_at']));
-
-            $repositories[] = $githubRepos;
-        }
-
-        return $repositories;
+        return array_map(
+            fn(array $item): GithubRepos => new GithubRepos(
+                [
+                    'name'           => $item['name'] ?? null,
+                    'github_user_id' => $user->getId(),
+                    'link'           => $item['html_url'],
+                    'updated_at'     => (new \DateTime($item['updated_at']))->format(DateFormat::SERVER_DATE_FORMAT),
+                ]
+            ),
+            $response->data
+        );
     }
 
     public function save(IGithubRepositoryEntity $githubRepositoryEntity): bool
